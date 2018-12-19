415eb7326b254a0f46ddffbe5f5e40f9190d9517
Disble <decoder@hotmail.es>
2018-12-18 23:58:29 -0500

    feat: change ubication for database, change name aplication

--- models/db-animes.js
+++ models/db-animes.js
@@ -1,5 +1,7 @@
+const settings = require('electron-settings');
 const Datastore = require('nedb');
 const path = require('path');
-const animesdb = new Datastore({filename: path.join(__dirname, '..', 'data', 'animes.dat'), autoload: true});
+const animesdb = new Datastore({filename: path.join(settings.file(), '..', 'data', 'animes.dat'), autoload: true});
+// const animesdb = new Datastore({filename: path.join(__dirname, '..', 'data', 'animes.dat'), autoload: true});
 
 exports.animesdb = animesdb;
--- models/db-pendientes.js
+++ models/db-pendientes.js
@@ -1,5 +1,7 @@
+const settings = require('electron-settings');
 const Datastore = require('nedb');
 const path = require('path');
-const pendientesdb = new Datastore({filename: path.join(__dirname, '..', 'data', 'pendientes.dat'), autoload: true});
+const pendientesdb = new Datastore({filename: path.join(settings.file(), '..', 'data', 'pendientes.dat'), autoload: true});
+// const pendientesdb = new Datastore({filename: path.join(__dirname, '..', 'data', 'pendientes.dat'), autoload: true});
 
 exports.pendientesdb = pendientesdb;
