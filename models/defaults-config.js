74645591f69913b22935593e24523daadb4e7dcd
Disble <decoder@hotmail.es>
2020-02-15 00:12:15 -0500

    refactor: delete old menu

--- models/defaults-config.js
+++ models/defaults-config.js
@@ -51,63 +51,6 @@ exports.Days = [
     }
 ]
 
-// exports.Menu = {
-//     'día': {
-//         'lunes': {
-//             'href': '#!',
-//             'id': "lunes",
-//             'class': 'collection-item no-link btn-buscar-animes'
-//         },
-//         'martes': {
-//             'href': '#!',
-//             'id': "martes",
-//             'class': 'collection-item no-link btn-buscar-animes'
-//         },
-//         'miércoles': {
-//             'href': '#!',
-//             'id': "miercoles",
-//             'class': 'collection-item no-link btn-buscar-animes'
-//         },
-//         'jueves': {
-//             'href': '#!',
-//             'id': "jueves",
-//             'class': 'collection-item no-link btn-buscar-animes'
-//         },
-//         'viernes': {
-//             'href': '#!',
-//             'id': "viernes",
-//             'class': 'collection-item no-link btn-buscar-animes'
-//         },
-//         'sábado': {
-//             'href': '#!',
-//             'id': "sabado",
-//             'class': 'collection-item no-link btn-buscar-animes'
-//         },
-//         'domingo': {
-//             'href': '#!',
-//             'id': "domingo",
-//             'class': 'collection-item no-link btn-buscar-animes'
-//         }
-//     },
-//     'estrenos': {
-//         'Sin ver': {
-//             'href': '#!',
-//             'id': "sin ver",
-//             'class': 'collection-item no-link btn-buscar-animes'
-//         },
-//         'Visto': {
-//             'href': '#!',
-//             'id': "visto",
-//             'class': 'collection-item no-link btn-buscar-animes'
-//         },
-//         'Ver hoy': {
-//             'href': '#!',
-//             'id': "ver hoy",
-//             'class': 'collection-item no-link btn-buscar-animes'
-//         }
-//     }
-// }
-
 exports.Estados = {
     'Viendo': 0,
     'Finalizado': 1,
